74b67ba1fc096b75a7a22e137c8417f692145b39
ProgramKingAbel <abelmorara254@gmail.com>
2023-04-26 17:39:36 +0100

    Generated works section dynamically using Javascript

--- script.js
+++ script.js
@@ -53,10 +53,107 @@ formReg.addEventListener('input', () => {
     });
 
     
+ //Dynamically generate the works section
+ 
+ //store data in js object
+
+const worksData = [
+    {
+        Company: "Tonic",
+        description: "A daily selection of privately personalized reads no accounts or sign-ups required.",
+        img: "/images/Tonic.svg",
+        technologies: ['HTML', 'CSS', 'Javascript'],
+        devName: "Canopy",
+        devRole: "Back End Dev",
+        year: 2015,
+        liveVersion:"https://programkingabel.github.io/",
+        sourceLink:"https://github.com/ProgramKingAbel/Abel-Morara-Portfolio",
+    },
+
+    {
+        Company: "Tonic",
+        description: "A daily selection of privately personalized reads no accounts or sign-ups required.",
+        img: "/images/Tonic.svg",
+        technologies: ['HTML', 'CSS', 'Javascript'],
+        devName: "Canopy",
+        devRole: "Back End Dev",
+        year: 2015,
+        liveVersion:"https://programkingabel.github.io/",
+        sourceLink:"https://github.com/ProgramKingAbel/Abel-Morara-Portfolio",
+    },
+
+    {
+        Company: "Tonic",
+        description: "A daily selection of privately personalized reads no accounts or sign-ups required.",
+        img: "/images/Tonic.svg",
+        technologies: ['HTML', 'CSS', 'Javascript'],
+        devName: "Canopy",
+        devRole: "Back End Dev",
+        year: 2015,
+        liveVersion:"https://programkingabel.github.io/",
+        sourceLink:"https://github.com/ProgramKingAbel/Abel-Morara-Portfolio",
+    },
+
+    {
+        Company: "Tonic",
+        description: "A daily selection of privately personalized reads no accounts or sign-ups required.",
+        img: "/images/Tonic.svg",
+        technologies: ['HTML', 'CSS', 'Javascript'],
+        devName: "Canopy",
+        devRole: "Back End Dev",
+        year: 2015,
+        liveVersion:"https://programkingabel.github.io/",
+        sourceLink:"https://github.com/ProgramKingAbel/Abel-Morara-Portfolio",
+    },
+
+    {
+        Company: "Tonic",
+        description: "A daily selection of privately personalized reads no accounts or sign-ups required.",
+        img: "/images/Tonic.svg",
+        technologies: ['HTML', 'CSS', 'Javascript', 'Ruby'],
+        devName: "Canopy",
+        devRole: "Back End Dev",
+        year: 2015,
+        liveVersion:"https://programkingabel.github.io/",
+        sourceLink:"https://github.com/ProgramKingAbel/Abel-Morara-Portfolio",
+    },
+];
 
 
-
+worksData.forEach((element, i) => {
     
+const projectsContainer = document.querySelector('.all-projects');
+// console.log(projectsContainer);
+
+//create card
+const card = document.createElement('div');
+card.classList = 'card';
+
+const projectCard = `
+            <div class="card-image">
+            <img src=${worksData[i].img}>
+            </div>
+            <div class="card-details">
+            <h2 class="title">${worksData[i].Company}</h2>
+            <ul class="developer-details">
+                <li>${worksData[i].devName}</li>
+                <span></span>
+                <li>${worksData[i].devRole}</li>
+                <span></span>
+                <li>${worksData[i].year}</li>
+            </ul>
+            <p>${worksData[i].description}</p>
+                <ul class="stack">
+                ${worksData[i].technologies.map((j) => `<li>${j}</li>`).join('')}
+                </ul>
+                <a href="#">See Project</a>
+            </div>
+
+`;
+card.innerHTML += projectCard;
+projectsContainer.appendChild(card);
+
+});
     
 
 
