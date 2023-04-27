3f9268327a314c0a5232605e31749831076bc778
ProgramKingAbel <abelmorara254@gmail.com>
2023-04-27 17:19:17 +0100

    Solved Stylelint errors

--- script.js
+++ script.js
@@ -1,145 +1,133 @@
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
 
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-    
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
 });
 
-document.querySelectorAll(".nav-link").forEach(i => i.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
+document.querySelectorAll('.nav-link').forEach((i) => i.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
 }));
 
 // Local Storage
 
- // retrieve the form and details
- 
+// retrieve the form and details
+
 const formReg = document.getElementById('register');
-const submitForm = document.getElementById('submit-btn');
 const userName = document.getElementById('name');
 const userEmail = document.getElementById('email');
 const userMessage = document.getElementById('message');
-   
 
 formReg.addEventListener('input', () => {
-    //get the values submitted as they are keyd in 
-    //store them in js object
-    const data = {
-        name: userName.value,
-        email: userEmail.value,
-        message: userMessage.value,
-    };
-    //stringify the data to store 
-    var storeData = JSON.stringify(data);
-    storeData = localStorage.setItem('data', storeData);
-        
+  // get the values submitted as they are keyd in
+  // store them in js object
+  const data = {
+    name: userName.value,
+    email: userEmail.value,
+    message: userMessage.value,
+  };
+    // stringify the data to store
+
+  const storeData = JSON.stringify(data);
+  localStorage.setItem('data', storeData);
 });
 
- //add event listener function when page is reloaded   
-        window.addEventListener("load", (event) => {
-//retrieve inforamtion
-        const userData = JSON.parse(localStorage.getItem('data'));
-    if (userData) {
-        const userName = document.getElementById('name');
-        const userEmail = document.getElementById('email');
-        const userMessage = document.getElementById('message');
-            userName.value = userData.name;
-            userEmail.value = userData.email;
-            userMessage.value = userData.email;
-        }
-        
-    });
+// add event listener function when page is reloaded
+window.addEventListener('load', () => {
+// retrieve inforamtion
+  const userData = JSON.parse(localStorage.getItem('data'));
+  if (userData) {
+    const userName = document.getElementById('name');
+    const userEmail = document.getElementById('email');
+    const userMessage = document.getElementById('message');
+    userName.value = userData.name;
+    userEmail.value = userData.email;
+    userMessage.value = userData.email;
+  }
+});
 
-    
- //Dynamically generate the works section
- 
- //store data in js object
+// Dynamically generate the works section
+
+// store data in js object
 
 const worksData = [
-    {
-        id: 1,
-        Company: "Tonic",
-        description: "A daily selection of privately personalized reads no accounts or sign-ups required.",
-        img: "/images/Tonic.svg",
-        technologies: ['HTML', 'CSS', 'Javascript'],
-        devName: "Canopy",
-        devRole: "Back End Dev",
-        year: 2015,
-        liveVersion:"https://programkingabel.github.io/",
-        sourceLink:"https://github.com/ProgramKingAbel/Abel-Morara-Portfolio",
-    },
+  {
+    id: 1,
+    Company: 'Tonic',
+    description: 'A daily selection of privately personalized reads no accounts or sign-ups required.',
+    img: '/images/Tonic.svg',
+    technologies: ['HTML', 'CSS', 'Javascript'],
+    devName: 'Canopy',
+    devRole: 'Back End Dev',
+    year: 2015,
+    liveVersion: 'https://programkingabel.github.io/',
+    sourceLink: 'https://github.com/ProgramKingAbel/Abel-Morara-Portfolio',
+  },
 
-    {
-        id: 2,
-        Company: "Tonic",
-        description: "A daily selection of privately personalized reads no accounts or sign-ups required.",
-        img: "/images/Tonic.svg",
-        technologies: ['HTML', 'CSS', 'Javascript'],
-        devName: "Canopy",
-        devRole: "Back End Dev",
-        year: 2015,
-        liveVersion:"https://programkingabel.github.io/",
-        sourceLink:"https://github.com/ProgramKingAbel/Abel-Morara-Portfolio",
-    },
+  {
+    id: 2,
+    Company: 'Multi-Post Stories',
+    description: 'A daily selection of privately personalized reads no accounts or sign-ups required.',
+    img: '/images/Nature.svg',
+    technologies: ['HTML', 'CSS', 'Javascript'],
+    devName: 'Canopy',
+    devRole: 'Back End Dev',
+    year: 2015,
+    liveVersion: 'https://programkingabel.github.io/',
+    sourceLink: 'https://github.com/ProgramKingAbel/Abel-Morara-Portfolio',
+  },
 
-    {
-        id: 3,
-        Company: "Tonic",
-        description: "A daily selection of privately personalized reads no accounts or sign-ups required.",
-        img: "/images/Tonic.svg",
-        technologies: ['HTML', 'CSS', 'Javascript'],
-        devName: "Canopy",
-        devRole: "Back End Dev",
-        year: 2015,
-        liveVersion:"https://programkingabel.github.io/",
-        sourceLink:"https://github.com/ProgramKingAbel/Abel-Morara-Portfolio",
-    },
+  {
+    id: 3,
+    Company: 'Facebook 360',
+    description: 'A daily selection of privately personalized reads no accounts or sign-ups required.',
+    img: '/images/Nature.svg',
+    technologies: ['HTML', 'CSS', 'Javascript'],
+    devName: 'Canopy',
+    devRole: 'Back End Dev',
+    year: 2015,
+    liveVersion: 'https://programkingabel.github.io/',
+    sourceLink: 'https://github.com/ProgramKingAbel/Abel-Morara-Portfolio',
+  },
 
-    {
-        id: 4,
-        Company: "Tonic",
-        description: "A daily selection of privately personalized reads no accounts or sign-ups required.",
-        img: "/images/Tonic.svg",
-        technologies: ['HTML', 'CSS', 'Javascript'],
-        devName: "Canopy",
-        devRole: "Back End Dev",
-        year: 2015,
-        liveVersion:"https://programkingabel.github.io/",
-        sourceLink:"https://github.com/ProgramKingAbel/Abel-Morara-Portfolio",
-    },
+  {
+    id: 4,
+    Company: 'Uber Navigation',
+    description: 'A daily selection of privately personalized reads no accounts or sign-ups required.',
+    img: '/images/grd2.svg',
+    technologies: ['HTML', 'CSS', 'Javascript', 'Ruby on Rails'],
+    devName: 'Uber',
+    devRole: 'Back End Dev',
+    year: 2018,
+    liveVersion: 'https://programkingabel.github.io/',
+    sourceLink: 'https://github.com/ProgramKingAbel/Abel-Morara-Portfolio',
+  },
 
-    {
-        id: 5,
-        Company: "Tonic",
-        description: "A daily selection of privately personalized reads no accounts or sign-ups required.",
-        img: "/images/Tonic.svg",
-        technologies: ['HTML', 'CSS', 'Javascript', 'Ruby'],
-        devName: "Canopy",
-        devRole: "Back End Dev",
-        year: 2015,
-        liveVersion:"https://programkingabel.github.io/",
-        sourceLink:"https://github.com/ProgramKingAbel/Abel-Morara-Portfolio",
-    },
+  {
+    id: 5,
+    Company: 'Tonic',
+    description: 'A daily selection of privately personalized reads no accounts or sign-ups required.',
+    img: '/images/Tonic.svg',
+    technologies: ['HTML', 'CSS', 'Javascript', 'Ruby'],
+    devName: 'Canopy',
+    devRole: 'Back End Dev',
+    year: 2015,
+    liveVersion: 'https://programkingabel.github.io/',
+    sourceLink: 'https://github.com/ProgramKingAbel/Abel-Morara-Portfolio',
+  },
 ];
 
+worksData.forEach((element, i) => {
+  const projectsContainer = document.querySelector('.all-projects');
+  // console.log(projectsContainer);
 
+  // create card
+  const card = document.createElement('div');
+  card.classList = 'card';
 
-
-    worksData.forEach((element, i) => {
-
-       
-    
-        const projectsContainer = document.querySelector('.all-projects');
-        // console.log(projectsContainer);
-
-        //create card
-        const card = document.createElement('div');
-        card.classList = 'card';
-
-        card.innerHTML = `
+  card.innerHTML = `
             <div class="card-image">
             <img src=${worksData[i].img}>
             </div>
@@ -160,44 +148,42 @@ const worksData = [
                          </div>
 
 `;
-        
-        projectsContainer.append(card);
 
-    });
+  projectsContainer.append(card);
+});
 
-
-
-
-
-
-   //open modal 
+// open modal
 const openModalBtn = document.querySelectorAll('[data-modal-target]');
 const closeModalBtn = document.querySelectorAll('[data-modal-close]');
 const overlay = document.getElementById('overlay');
 
+openModalBtn.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    const modal = document.querySelector(btn.dataset.modalTarget);
+    let { id } = e.target;
+    id = parseInt(id, 10);
 
+    // console.log(id);
+    // search for data in array
+    const getItem = worksData.find((i) => i.id === id);
+    // console.log(getItem);
 
-openModalBtn.forEach(btn => {
-    btn.addEventListener('click', (e) => {
+    // generate popup
 
-        const modal = document.querySelector(btn.dataset.modalTarget);
-        let { id } = e.target;    
-        id = parseInt(id);
+    const modalContainer = document.querySelector('.modal-wrapper');
+    // console.log(modalContainer);
 
-        // console.log(id);
-       //search for data in array
-       const getItem = worksData.find((i) => i.id === id);
-        // console.log(getItem);
+    // open pop up
+    function openModal(modal) {
+      if (modal == null) return;
+      modal.classList.add('active');
+      overlay.classList.add('active');
+    }
 
-        //generate popup
+    const modalCard = document.createElement('div');
+    modalCard.classList = 'modal-card';
 
-        const modalContainer = document.querySelector('.modal-wrapper');
-// console.log(modalContainer);
-
-const modalCard = document.createElement('div');
-modalCard.classList = 'modal-card';
-
-modalCard.innerHTML = ` 
+    modalCard.innerHTML = ` 
 
         <div class="modal-header">
         <h2 class="title">${getItem.Company}</h2>
@@ -234,49 +220,33 @@ modalCard.innerHTML = `
         
 
 `;
-        
-        modalContainer.append(modalCard);
 
-        openModal(modal);
-    })
-})
-closeModalBtn.forEach(btn => {
-    btn.addEventListener('click', (e) => {
-        
-        const modal = btn.closest('.modal');
-        // console.log(modal);
-        closeModal(modal);
-    });
+    modalContainer.append(modalCard);
+
+    openModal(modal);
+  });
+});
+
+// close pop up
+function closeModal(modal) {
+  if (modal == null) return;
+  modal.innerHTML = '';
+  modal.classList.remove('active');
+  overlay.classList.remove('active');
+}
+
+closeModalBtn.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    const modal = document.querySelector('.modal-wrapper.active');
+    // const modal = btn.closest('.modal');
+    // console.log(modal);
+    closeModal(modal);
+  });
 });
 
 overlay.addEventListener('click', () => {
-    const modals = document.querySelectorAll('.modal-wrapper.active');
-    modals.forEach(modal => {
-
-        closeModal(modal);
-    });
-    
+  const modals = document.querySelectorAll('.modal-wrapper.active');
+  modals.forEach((modal) => {
+    closeModal(modal);
+  });
 });
-
-//open pop up
-function openModal(modal) {
-    if (modal == null) return
-    modal.classList.add('active');
-    overlay.classList.add('active');
-    
-}
-
-//close pop up
-function closeModal(modal) {
-    if (modal == null) return
-    modal.innerHTML = '';
-    modal.classList.remove('active');
-    overlay.classList.remove('active');
-    
-}
-    
-   
-
-
-    
- 
